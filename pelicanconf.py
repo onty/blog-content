8ffc4ee150b9973b2fc4ec9ae4651765451532fb
U-ERICSSON\elinpra <elinpra@E7FC15B4EA501F.ericsson.se>
2014-11-23 22:53:46 +0800

    add static path

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,7 +18,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
+STATIC_PATHS = ['images']
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
