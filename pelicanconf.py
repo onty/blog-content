72c57ed1ca50077359f016c36cc577d69e3ee270
U-ERICSSON\elinpra <elinpra@E7FC15B4EA501F.ericsson.se>
2014-12-11 14:18:07 +0800

    add travel blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,7 +24,7 @@ AUTHOR_FEED_RSS = None
 STATIC_PATHS = ['images']
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
+         ('My Travelling Logs', 'http://travel.theprasojos.web.id'),
          ('Blognya TePot', 'https://vhynaulia.wordpress.com/'),
          ('Theprasojos Family', 'http://theprasojos.wordpress.com'),)
 
