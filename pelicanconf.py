b2bf08d816ead8177d4a48d508a9450151580859
Lintang Prasojo <lintang.jp@gmail.com>
2021-08-05 17:20:59 +0700

    update blog 2021

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,13 +3,12 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Lintang JP'
-SITENAME = u"JPrasojo's blog"
+SITENAME = u"LJP's blog"
 SITEURL = ''
-#THEME = "pelican-themes/bootstrap"
-THEME = "pelican-themes/plumage"
-#THEME = "../pelican-themes/pelican-sober"
+#THEME = "pelican-themes/plumage"
+THEME = "pelican-themes/pelican-sober"
 PELICAN_SOBER_ABOUT = "Just an ordinary telecom engineer, working for Online Charging System.."
-#PELICAN_SOBER_STICKY_SIDEBAR = True
+PELICAN_SOBER_STICKY_SIDEBAR = True
 #BOOTSTRAP_THEME = "lovers"
 #THEME = "pelican-themes/fresh"
 PATH = 'content'
@@ -19,8 +18,9 @@ TIMEZONE = 'Europe/Dublin'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
@@ -40,3 +40,17 @@ articles_paginator = 6
 SITEMAP_SAVE_AS = 'sitemap.xml'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+DISQUS_SITENAME = "jprasojos"
+GOOGLE_ANALYTICS = "UA-56589451-1"
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+DAY_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/index.html'
+
+USE_FOLDER_AS_CATEGORY = True
+DISPLAY_CATEGORIES_ON_MENU = True
+DISPLAY_PAGES_ON_MENU = True
